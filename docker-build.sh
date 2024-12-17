60e3699de32354f5a60f35504af43e8197b3916c
Kim-MyungJin <myungjin.kim@switchwon.com>
2024-12-17 18:46:49 +0900

    음 이미지 버전은 어떻게 해야할까

--- docker-build.sh
+++ docker-build.sh
@@ -1,4 +1,4 @@
 #!/bin/zsh
 
 #docker buildx build --platform=linux/amd64,linux/arm64 -t kimmyungjin/kube-spring-app .
-docker buildx build --platform=linux/amd64 -t kimmyungjin/kube-spring-app .
+docker buildx build --platform=linux/amd64 -t kimmyungjin/kube-spring-app:2 .
